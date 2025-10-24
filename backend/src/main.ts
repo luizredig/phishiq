27b992a39f1cdbd11f6ccdbc255493ca55fd804e
redig <luizredig@gmail.com>
2025-10-24 13:51:28 -0300

    chore(api): adiciona prefixo

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,6 +13,8 @@ async function bootstrap() {
     credentials: true,
   })
 
+  app.setGlobalPrefix('/api')
+
   // Bull Board em /admin/queues
   const serverAdapter = new ExpressAdapter()
   serverAdapter.setBasePath('/admin/queues')
